f528255a5560d4c2c5bc6dd0948cc87b03888439
sweemeng <sweester@gmail.com>
2012-03-10 17:55:50 +0800

    added views for F&B, Hotel, and tour

--- collective/barcamp/browser/eventview.py
+++ collective/barcamp/browser/eventview.py
@@ -10,3 +10,19 @@ class BarcampSessionView(BrowserView):
 class BarcampAttendanceView(BrowserView):
     def get_attendee(self):
         return self.context.registrations.items()
+
+
+class BarcampFNBView(BrowserView):
+    def get_attendee(self):
+        return self.context.registrations.items()
+
+
+class BarcampHotelView(BrowserView):
+    def get_attendee(self):
+        return self.context.registrations.items()
+
+
+class BarcampTourView(BrowserView):
+    def get_attendee(self):
+        return self.context.registrations.items()
+
